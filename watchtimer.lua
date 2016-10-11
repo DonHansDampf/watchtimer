09d1063cc3e4c156b755c4eb16239f7c997c61c9
Marcel Kessler <donhansdampf@posteo.de>
2016-10-11 21:19:35 +0200

    switched to os.date and os.time

--- watchtimer.lua
+++ watchtimer.lua
@@ -9,53 +9,31 @@ function osd(str)
   return mp.osd_message(str, 3)
 end
 
-function timestamp(duration)
-  local hours = duration / 3600
-  local minutes = duration % 3600 / 60
-  local seconds = duration % 60
-  return string.format("%02d:%02d:%02.03f",
-                       hours,
-                       minutes,
-                       seconds)
-end
-
 function timer_start()
-  local time_hour = os.date("%H")
-  local time_min = os.date("%M")
-  local time_sec = os.date("%S")
-  local time = os.date("%X")
+  local time_int = os.time()
+  local time_string = os.date("%X", time_int)
 
-  if start_time then
-    local end_time_hour = time_hour
-    local end_time_min = time_min
-    local end_time_sec = time_sec
-    local end_time = time
+  if start_time_int then
+    local end_time_int = time_int
+    local end_time_string = time_string
 
     -- calculate watchtime
-    local watch_time_hour = end_time_hour - start_time_hour
-    local watch_time_min = end_time_min - start_time_min
-    local watch_time_sec = end_time_sec - start_time_sec
-    local watch_time = string.format("%02d:%02d:%02d",
-                                     watch_time_hour,
-                                     watch_time_min,
-                                     watch_time_sec
-    )
+    local watch_time_int = os.difftime(end_time_int, start_time_int)
+    local watch_time_string = os.date("!%X", watch_time_int)
 
     osd(string.format("Watched from %s till %s; Duration: %s",
-                      start_time,
-                      end_time,
-                      watch_time
+                      start_time_string,
+                      end_time_string,
+                      watch_time_string
     ))
 
-    start_time_hour, start_time_min, start_time_sec, start_time = nil
+    start_time_int = nil
   else
-    start_time_hour = time_hour
-    start_time_min = time_min
-    start_time_sec = time_sec
-    start_time = time
+    start_time_int = time_int
+    start_time_string = time_string
 
     osd(string.format("Started watching at: %s",
-                      start_time
+                      start_time_string
     ))
   end
 end
